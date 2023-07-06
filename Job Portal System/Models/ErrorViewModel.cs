0a46b498379893c734d2d05bf0e8e10102c63edc
suniksha-karki <karkisuniksha555@gmail.com>
2023-07-06 19:37:28 +0545

    Add project files.

---
+++ Job Portal System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Job_Portal_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
